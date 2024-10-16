45483ed3ddb2b724d59d007e1d492ac658a167a0
Abdulmalikoye <106560197+Abdulmalikoye@users.noreply.github.com>
2024-10-16 15:50:59 +0100

    Save Progress on master branch

--- src/components/component/ArtGallery.jsx
+++ src/components/component/ArtGallery.jsx
@@ -1,7 +1,9 @@
 // src/pages/ArtGallery.js
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { fetchArtworkData } from "../../FirebaseFunctions";
-import ArtCard from "./ArtCard";
+// import ArtCard from "./ArtCard";
+import { BeatLoader } from "react-spinners";
+const ArtCard = lazy(() => import("./ArtCard"));
 
 const ArtGallery = () => {
   const [artworks, setArtworks] = useState([]);
@@ -15,18 +17,20 @@ const ArtGallery = () => {
   }, []);
 
   return (
-    <div className="grid md:grid-cols-3 grid-cols-1 gap-8 w-full">
-      {artworks.map((art, index) => (
-        <ArtCard
-          key={index}
-          title={art.title}
-          description={art.description}
-          price={art.price}
-          imageUrl={art.imageUrl}
-          artwork={art}
-        />
-      ))}
-    </div>
+    <Suspense fallback={<BeatLoader color="blue" />}>
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-8 w-full">
+        {artworks.map((art, index) => (
+          <ArtCard
+            key={index}
+            title={art.title}
+            description={art.description}
+            price={art.price}
+            imageUrl={art.imageUrl}
+            artwork={art}
+          />
+        ))}
+      </div>
+    </Suspense>
   );
 };
 
